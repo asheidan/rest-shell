7e38aade0f7aa522ffb1a46474606f68227ef649
Emil Eriksson <emil.eriksson@codemill.se>
2015-03-10 11:30:15 +0100

    Makes the script respect exported vars

--- rest-shell.bash
+++ rest-shell.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 
-export REST_PROTO="http"
+export REST_PROTO=${REST_PROTO:-"http"}
 function proto() { REST_PROTO="${1}"; }
 typeset -fx proto
 
-export REST_HOST="localhost"
+export REST_HOST=${REST_HOST:-"localhost"}
 function host() { REST_HOST="${1}"; }
 typeset -fx host
 
@@ -16,7 +16,7 @@ export REST_PASS
 function pass() { REST_PASS="${1}"; }
 typeset -fx pass
 
-export REST_BASE="/"
+export REST_BASE=${REST_BASE:-"/"}
 function rcd() {
 	# TODO: error on more than one argument (or loop?)
 	if [[ ".." = "${1}" ]]; then
@@ -38,10 +38,10 @@ function _curl() {
 	rest_method="${1} ${2}"
 	shift 2
 	if [[ "${REST_USER}" ]]; then
-		rest_auth="-u '${REST_USER}'"
+		rest_auth="-u ${REST_USER}"
 	fi
 	if [[ "${REST_PASS}" ]]; then
-		rest_auth="${rest_auth}:'${REST_PASS}'"
+		rest_auth="${rest_auth}:${REST_PASS}"
 	fi
 
 	rest_url="${REST_PROTO}://${REST_HOST}${REST_BASE}"
@@ -50,10 +50,12 @@ function _curl() {
 	fi
 
 	if [[ "${REST_ACCEPT}" ]]; then
-		rest_accept="-H '${REST_ACCEPT}'"
+		rest_accept="-H 'Accept: ${REST_ACCEPT}'"
 	fi
 	
-	echo curl $rest_auth $rest_method $rest_accept $rest_url
+	curl_command="curl ${rest_auth} ${rest_method} ${rest_accept} ${rest_url}"
+	echo "${curl_command}"
+	${curl_command}
 }
 typeset -fx _curl
 
@@ -65,13 +67,22 @@ typeset -fx get
 
 function prompt() {
 	if [[ "${REST_USER}" ]]; then
-		echo "${REST_PROTO}://${REST_USER}@${REST_HOST}${REST_BASE}"
+		prompt_string="${REST_PROTO}://${REST_USER}@${REST_HOST}${REST_BASE}"
 	else
-		echo "${REST_PROTO}://${REST_HOST}${REST_BASE}"
+		prompt_string="${REST_PROTO}://${REST_HOST}${REST_BASE}"
 	fi
+
+	echo $prompt_string
 }
 typeset -fx prompt
 
-export PS1="\[\e[45m\e[1;35m\][\[\e[1;37m\]\$(prompt)\[\e[1;35m\]]\[\e[0m\]\n"
+function _pg(){
+	first="${1}"
+	shift
+	echo '\[\e\e[1;35m\]['"${first}"'\[\e[1;37m\]'"${*}"'\[\e[1;35m\]]'
+}
+typeset -fx _pg
+
+export PS1="\[\e[45m\]$(_pg '' '$(prompt)')$(_pg 'A:' \${REST_ACCEPT})    $(_pg '' '\w')\[\e[0m\]\n"
 
 exec bash
